9a4c24fb72b7f968d9ebc12fd2ba2422f85f99c1
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2014-10-09 21:10:28 -0700

    Added node bencoding.

--- lib/kademlia/node.rb
+++ lib/kademlia/node.rb
@@ -1,3 +1,5 @@
+require 'bencode'
+
 class Node
   attr_reader :peer_id
   attr_reader :ip
@@ -7,5 +9,9 @@ class Node
     @peer_id = peer_id
     @ip = ip
     @port = port
+  end
+
+  def bencode
+    {"peer_id" => @peer_id, "ip" => @ip, "port" => @port}.bencode
   end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -3,15 +3,24 @@ require_relative '../lib/kademlia/node'
 
 describe Node do
 
-  it "it must have a peer_id, ip, and port" do
+  it "must have a peer_id, ip, and port" do
     peer_id = 1
     ip = "127.0.0.1"
     port = 6881
-
     node = Node.new(peer_id, ip, port)
+
     node.peer_id.must_equal peer_id
     node.ip.must_equal ip
     node.port.must_equal port 
+  end
+
+  it "must be bencodable" do
+    peer_id = 1
+    ip = "127.0.0.1"
+    port = 6881
+    node = Node.new(peer_id, ip, port)
+
+    node.bencode.must_equal "d2:ip9:127.0.0.17:peer_idi1e4:porti6881ee"
   end
 
 end
