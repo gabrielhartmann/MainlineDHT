263268b09fb273cfd6a6ac010036e079d7870b72
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2014-10-11 13:28:08 -0700

    Made the default ID space for the RoutingTable a class variable and updated the tests accordingly.

--- lib/kademlia/routing_table.rb
+++ lib/kademlia/routing_table.rb
@@ -1,14 +1,18 @@
 require_relative 'bucket'
 
 class RoutingTable
-  Default_Id_Space = 160
-
   attr_reader :id_space
   attr_reader :buckets
 
-  def initialize (id_space = Default_Id_Space)
+  @@Default_Id_Space = 160
+
+  def initialize (id_space = @@Default_Id_Space)
     @id_space = id_space
     @buckets = Array.new(1) {Bucket.new(id_space)}
+  end
+
+  def self.default_id_space
+    @@Default_Id_Space
   end
 
 end
--- test/routing_table_test.rb
+++ test/routing_table_test.rb
@@ -4,10 +4,10 @@ require_relative '../lib/kademlia/routing_table'
 describe RoutingTable do
   it "can be created with a default id space" do
     routing_table = RoutingTable.new
-    routing_table.id_space.must_equal RoutingTable::Default_Id_Space
+    routing_table.id_space.must_equal RoutingTable::default_id_space
   end
 
-  it "can be created witha a custom id space" do
+  it "can be created with a custom id space" do
     id_space = 3
     routing_table = RoutingTable.new(id_space)
     routing_table.id_space.must_equal id_space
