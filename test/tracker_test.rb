931c4cde7f1d1e4a101aed70a95377085cb795c3
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2014-10-18 21:45:00 -0700

    Fixed the description of the tracker test.

--- test/tracker_test.rb
+++ test/tracker_test.rb
@@ -4,7 +4,7 @@ require_relative 'test_helper'
 require_relative '../lib/kademlia/tracker'
 
 describe Tracker do
-  it "can process a torrent file" do
+  it "can encode the info section of a torrent file" do
     t = Tracker.new('ubuntu.torrent')
     CGI.escape(Digest::SHA1.digest(t.metainfo['info'].bencode)).must_equal t.hashed_info
   end
