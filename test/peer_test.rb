1d2f34269bd3099b30f03d4adb9b15e0301d5475
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2014-10-24 21:34:06 -0700

    Peers now shake hands without arguments.

--- test/peer_test.rb
+++ test/peer_test.rb
@@ -1,22 +1,27 @@
 require_relative 'test_helper'
+require_relative 'torrent_test_helper'
 require_relative '../lib/kademlia/peer'
 
 describe Peer do
   test_id = "abcdefghijklmnoprstu"
   test_ip = "127.0.0.1"
   test_port = 6881
+  test_hashed_info = 12345678901234567890
+  test_local_peer_id = "abcdefghijklmnopqrst"
 
   it "must create an id 20 bytes long if none is specified" do
-    peer = Peer.new(test_ip, test_port)
+    peer = Peer.new(test_ip, test_port, test_hashed_info, test_local_peer_id)
     peer.id.length.must_equal 20
   end 
 
   it "must fail to create a node with an invalid id" do
-    assert_raises(InvalidPeerError) { peer = Peer.new(test_ip, test_port, test_id + "1") }
+    assert_raises(InvalidPeerError) { peer = Peer.new(test_ip, test_port, test_hashed_info, test_local_peer_id, test_id + "1") }
   end
 
   it "must be able to shake hands" do
-    response = Peer.shake_hands
-    
+    t = Torrent.default
+    p = t.peers.first
+    response = p.shake_hands
+    puts response
   end
 end
