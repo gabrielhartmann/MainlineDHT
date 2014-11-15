fe5f8dcb5d4514e0c3d15ac8220b26e2dc7c4e36
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2014-11-15 14:50:23 -0800

    Respond state machine complete.

--- lib/kademlia/peer_respond_state_machine.rb
+++ lib/kademlia/peer_respond_state_machine.rb
@@ -2,7 +2,7 @@ require 'workflow'
 require_relative 'peer'
 
 class PeerRespondStateMachine
-  attr_reader :am_chocking
+  attr_reader :am_choking
   attr_reader :peer_interested
 
   def initialize
@@ -19,36 +19,37 @@ class PeerRespondStateMachine
   end
 
   def send_choke
-    @am_chocking = true
+    @am_choking = true
   end
 
   def send_unchoke
-    @am_chocking = false
+    @am_choking = false
   end
 
   include Workflow
   workflow do
     state :neutral do
-      event :recv_interested, :transitions_to => :dont_respond
+      event :recv_interested, :transitions_to => :wait_unchoke
+      event :send_unchoke, :transitions_to => :wait_interest
     end
 
-    state :dont_respond do
+    state :wait_unchoke do
       event :recv_not_interested, :transitions_to => :neutral
       event :send_unchoke, :transitions_to => :respond
-
-      after_transition do
-	# pass through this state if we are already unchoked
-	send_unchoke! unless @am_choking
-      end
     end
 
     state :respond do
-      event :send_choke, :transitions_to => :dont_respond
-      event :recv_not_interested, :transitions_to => :neutral
+      event :send_choke, :transitions_to => :wait_unchoke
+      event :recv_not_interested, :transitions_to => :wait_interest
+    end
+
+    state :wait_interest do
+      event :recv_interested, :transitions_to => :respond
+      event :send_choke, :transitions_to => :neutral
     end
 
     on_transition do |from, to, triggering_event, *event_args|
-      # puts "#{triggering_event}: #{from} -> #{to}"
+     # puts "#{triggering_event}: #{from} -> #{to}"
     end
   end
 end 
--- test/peer_respond_state_machine_test.rb
+++ test/peer_respond_state_machine_test.rb
@@ -4,23 +4,97 @@ require_relative '../lib/kademlia/peer_respond_state_machine'
 
 describe PeerRespondStateMachine do
 
-  it "must be able to transition from neutral to dont_respond and back" do
+  it "must be able to transition from neutral to wait_unchoke and back" do
     # Initialization is as expected
     test_machine = PeerRespondStateMachine.new
     test_machine.neutral?.must_equal true
     test_machine.peer_interested.must_equal false 
 
-    # Transition from neutral to dont_respond
+    # Transition from neutral to wait_unchoke
     test_machine.can_recv_interested?.must_equal true
     test_machine.recv_interested!
-    test_machine.dont_respond?.must_equal true
+    test_machine.wait_unchoke?.must_equal true
     test_machine.peer_interested.must_equal true
 
-    # Transition from dont_respond to neutral
+    # Transition from wait_unchoke to neutral
     test_machine.can_recv_not_interested?.must_equal true
     test_machine.recv_not_interested!
     test_machine.neutral?.must_equal true
     test_machine.peer_interested.must_equal false
   end 
+
+  it "must be able to transition from neutral to wait_unchoke to respond to wait_unchoke" do
+    # Initialization is as expected
+    test_machine = PeerRespondStateMachine.new
+    test_machine.neutral?.must_equal true
+    test_machine.peer_interested.must_equal false 
+
+    # Transition from neutral to wait_unchoke
+    test_machine.can_recv_interested?.must_equal true
+    test_machine.recv_interested!
+    test_machine.wait_unchoke?.must_equal true
+    test_machine.peer_interested.must_equal true
+
+    # Transition from wait_unchoke to respond
+    test_machine.can_send_unchoke?.must_equal true 
+    test_machine.send_unchoke!
+    test_machine.respond?.must_equal true
+    test_machine.am_choking.must_equal false
+
+    # Transition from respond to wait_unchoke
+    test_machine.can_send_choke?.must_equal true
+    test_machine.send_choke!
+    test_machine.wait_unchoke?.must_equal true
+    test_machine.am_choking.must_equal true
+  end
   
+  it "must be able to transition from neutral to wait_unchoke to respond to wait_interest to respond" do
+    # Initialization is as expected
+    test_machine = PeerRespondStateMachine.new
+    test_machine.neutral?.must_equal true
+    test_machine.peer_interested.must_equal false 
+
+    # Transition from neutral to wait_unchoke
+    test_machine.can_recv_interested?.must_equal true
+    test_machine.recv_interested!
+    test_machine.wait_unchoke?.must_equal true
+    test_machine.peer_interested.must_equal true
+
+    # Transition from wait_unchoke to respond
+    test_machine.can_send_unchoke?.must_equal true 
+    test_machine.send_unchoke!
+    test_machine.respond?.must_equal true
+    test_machine.am_choking.must_equal false
+
+    # Transition from respond to wait_interest
+    test_machine.can_recv_not_interested?.must_equal true
+    test_machine.recv_not_interested!
+    test_machine.wait_interest?.must_equal true
+    test_machine.peer_interested.must_equal false
+    test_machine.can_recv_interested?.must_equal true
+    test_machine.recv_interested!
+    test_machine.respond?.must_equal true
+    test_machine.peer_interested.must_equal true
+  end
+
+  it "must be able to transition from neutral to wait_interest to neutral" do
+    # Initialization is as expected
+    test_machine = PeerRespondStateMachine.new
+    test_machine.neutral?.must_equal true
+    test_machine.peer_interested.must_equal false 
+
+    # Transition from neutral to wait_interest
+    test_machine.can_send_unchoke?.must_equal true
+    test_machine.send_unchoke!
+    test_machine.wait_interest?.must_equal true
+    test_machine.am_choking.must_equal false
+    test_machine.peer_interested.must_equal false
+
+    # Transition from wait_interest to neutral
+    test_machine.can_send_choke?.must_equal true
+    test_machine.send_choke!
+    test_machine.neutral?.must_equal true
+    test_machine.am_choking.must_equal true
+    test_machine.peer_interested.must_equal false
+  end
 end 
